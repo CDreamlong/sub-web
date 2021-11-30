7516abe1b0486b8dcb916fd3e23c0c94c5effccd
CDreamlong <77619558+CDreamlong@users.noreply.github.com>
2021-11-30 15:12:55 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -551,7 +551,7 @@ export default {
   },
   mounted() {
     this.form.clientType = "clash";
-    this.form.customBackend = defaultBackend;
+    this.form.customBackend = "httos://now-subconverter-wine.vercel.app/sub?";
     this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
     this.getBackendVersion();
   },
